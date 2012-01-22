760edde361059b771f2d4c484f7287c327565b07
Antony Martin <martin.antony@yahoo.fr>
2012-01-22 01:48:14 +0100

    Added a basic uniform grid module

---
+++ include/SCE/core/SCEGrid.h
@@ -0,0 +1,70 @@
+/*------------------------------------------------------------------------------
+    SCEngine - A 3D real time rendering engine written in the C language
+    Copyright (C) 2006-2012  Antony Martin <martin(dot)antony(at)yahoo(dot)fr>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -----------------------------------------------------------------------------*/
+
+/* created: 21/01/2012
+   updated: 22/01/2012 */
+
+#ifndef SCEGRID_H
+#define SCEGRID_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef size_t (*SCE_FSerializeGrid) (void*, void*);
+
+typedef struct sce_sgrid SCE_SGrid;
+struct sce_sgrid {
+    void *data;
+    int width, height, depth;
+    SCE_EType type;
+    int built;
+    void *udata;
+};
+
+void SCE_Grid_Init (SCE_SGrid*);
+void SCE_Grid_Clear (SCE_SGrid*);
+SCE_SGrid* SCE_Grid_Create (void);
+void SCE_Grid_Delete (SCE_SGrid*);
+
+void SCE_Grid_SetType (SCE_SGrid*, SCE_EType);
+void SCE_Grid_SetDimensions (SCE_SGrid*, int, int, int);
+void SCE_Grid_SetWidth (SCE_SGrid*, int);
+void SCE_Grid_SetHeight (SCE_SGrid*, int);
+void SCE_Grid_SetDepth (SCE_SGrid*, int);
+
+SCE_EType SCE_Grid_GetType (const SCE_SGrid*);
+void* SCE_Grid_GetRaw (SCE_SGrid*);
+int SCE_Grid_GetWidth (const SCE_SGrid*);
+int SCE_Grid_GetHeight (const SCE_SGrid*);
+int SCE_Grid_GetDepth (const SCE_SGrid*);
+int SCE_Grid_GetNumPoints (const SCE_SGrid*);
+size_t SCE_Grid_GetSize (const SCE_SGrid*);
+
+void SCE_Grid_SetData (SCE_SGrid*, void*);
+void* SCE_Grid_GetData (SCE_SGrid*);
+
+int SCE_Grid_Build (SCE_SGrid*);
+
+void SCE_Grid_Serialize (SCE_SGrid*, SCE_FSerializeGrid, void*);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* guard */
---
+++ src/SCEGrid.c
@@ -0,0 +1,134 @@
+/*------------------------------------------------------------------------------
+    SCEngine - A 3D real time rendering engine written in the C language
+    Copyright (C) 2006-2012  Antony Martin <martin(dot)antony(at)yahoo(dot)fr>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -----------------------------------------------------------------------------*/
+
+/* created: 21/01/2012
+   updated: 22/01/2012 */
+
+#include <SCE/utils/SCEUtils.h>
+
+#include "SCE/core/SCEGrid.h"
+
+void SCE_Grid_Init (SCE_SGrid *grid)
+{
+    grid->data = NULL;
+    grid->width = grid->height = grid->depth = 0;
+    grid->type = SCE_BYTE;
+    grid->built = SCE_FALSE;
+    grid->udata = NULL;
+}
+void SCE_Grid_Clear (SCE_SGrid *grid)
+{
+    SCE_free (grid->data);
+}
+SCE_SGrid* SCE_Grid_Create (void)
+{
+    SCE_SGrid *grid = NULL;
+    if (!(grid = SCE_malloc (sizeof *grid)))
+        SCEE_LogSrc ();
+    else
+        SCE_Grid_Init (grid);
+    return grid;
+}
+void SCE_Grid_Delete (SCE_SGrid *grid)
+{
+    if (grid) {
+        SCE_Grid_Clear (grid);
+        SCE_free (grid);
+    }
+}
+
+void SCE_Grid_SetType (SCE_SGrid *grid, SCE_EType type)
+{
+    grid->type = type;
+}
+void SCE_Grid_SetDimensions (SCE_SGrid *grid, int width, int height, int depth)
+{
+    grid->width = width;
+    grid->height = height;
+    grid->depth = depth;
+}
+void SCE_Grid_SetWidth (SCE_SGrid *grid, int width)
+{
+    grid->width = width;
+}
+void SCE_Grid_SetHeight (SCE_SGrid *grid, int height)
+{
+    grid->height = height;
+}
+void SCE_Grid_SetDepth (SCE_SGrid *grid, int depth)
+{
+    grid->depth = depth;
+}
+
+SCE_EType SCE_Grid_GetType (const SCE_SGrid *grid)
+{
+    return grid->type;
+}
+void* SCE_Grid_GetRaw (SCE_SGrid *grid)
+{
+    return grid->data;
+}
+int SCE_Grid_GetWidth (const SCE_SGrid *grid)
+{
+    return grid->width;
+}
+int SCE_Grid_GetHeight (const SCE_SGrid *grid)
+{
+    return grid->height;
+}
+int SCE_Grid_GetDepth (const SCE_SGrid *grid)
+{
+    return grid->depth;
+}
+int SCE_Grid_GetNumPoints (const SCE_SGrid *grid)
+{
+    return grid->width * grid->height * grid->depth;
+}
+size_t SCE_Grid_GetSize (const SCE_SGrid *grid)
+{
+    return SCE_Type_Sizeof (grid->type) * SCE_Grid_GetNumPoints (grid);
+}
+
+void SCE_Grid_SetData (SCE_SGrid *grid, void *udata)
+{
+    grid->udata = udata;
+}
+void* SCE_Grid_GetData (SCE_SGrid *grid)
+{
+    return grid->udata;
+}
+
+
+int SCE_Grid_Build (SCE_SGrid *grid)
+{
+    size_t size;
+
+    if (grid->built)
+        return SCE_OK;
+
+    size = SCE_Grid_GetSize (grid);
+    if (!(grid->data = SCE_malloc (size)))
+        goto fail;
+
+    /* TODO: fillup the grid with something */
+
+    return SCE_OK;
+fail:
+    SCEE_LogSrc ();
+    return SCE_ERROR;
+}
